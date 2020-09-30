5452a867c9804658ab7c4970219d5287b36e3d3c
NOMBRE APELLIDO <correo@foo.com>
2020-09-30 13:16:31 -0300

    Archivos php actualizados

--- Sites/consultas/consulta_buques_max_personas.php
+++ Sites/consultas/consulta_buques_max_personas.php
@@ -18,14 +18,11 @@
       <th>ID</th>
       <th>ID Naviera</th>
       <th>Nombre</th>
-      <th>Patente</th>
-      <th>País</th>
-      <th>Tipo</th>
-      <th>ID Capitán</th>
+      <th>Cantidad de trabajadores</th>
     </tr>
   <?php
 	foreach ($buques as $b) {
-  		echo "<tr><td>$b[0]</td><td>$b[1]</td><td>$b[2]</td><td>$b[3]</td><td>$b[4]</td><td>$b[5]</td><td>$b[6]</td><</tr>";
+  		echo "<tr><td>$b[0]</td><td>$b[1]</td><td>$b[2]</td><td>$b[3]</td></tr>";
 	}
   ?>
 	</table>
