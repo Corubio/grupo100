44485e8b091315217080a6250e05789469e77382
NOMBRE APELLIDO <correo@foo.com>
2020-09-30 14:13:53 -0300

    Archivos php actualizados

--- Sites/consultas/consulta_buques_mejillones.php
+++ Sites/consultas/consulta_buques_mejillones.php
@@ -23,26 +23,24 @@
 
 	<table>
     <tr>
+      <th>Fecha Entrada Buque Solicitado</th>
+      <th>Fecha Salida Buque Solicitado</th>
+      <th>ID Buque</th>
+      <th>ID Naviera</th>
+      <th>Nombre Buque</th>
+      <th>Patente</th>
+      <th>País</th>
+      <th>Tipo</th>
+      <th>ID Capitán</th>
+      <th>ID Atraque</th>
+      <th>Puerto</th>
       <th>Fecha Entrada</th>
       <th>Fecha Salida</th>
-      <th>ID Buque Solicitado</th>
-      <th>ID Naviera de Buque Solicitado </th>
-      <th>Nombre Buque Solicitado</th>
-      <th>Patente Buque Solicitado/th>
-      <th>País Buque Solicitado</th>
-      <th>Tipo Buque Solicitado</th>
-      <th>ID Capitán Buque Solicitado</th>
-      <th>ID Atraque</th>
-      <th>ID Capitán</th>
-      <th>ID Capitán</th>
-      <th>ID Capitán</th>
-      <th>ID Capitán</th>
-      <th>ID Capitán</th>
     </tr>
   <?php
 	foreach ($buques as $b) {
       echo "<tr><td>$b[0]</td><td>$b[1]</td><td>$b[2]</td><td>$b[3]</td><td>$b[4]</td><td>$b[5]</td><td>$b[6]</td><td>$b[7]
-      </td><td>$b[8]</td><td>$b[9]</td><td>$b[10]</td><td>$b[11]</td><td>$b[12]</td><td>$b[13]</td><td>$b[14]</td></tr>";
+      </td><td>$b[8]</td><td>$b[9]</td><td>$b[10]</td><td>$b[11]</td><td>$b[12]</td></tr>";
 	}
   ?>
 	</table>
