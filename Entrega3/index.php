a9e40e99af5d837bdf4327dd1282784b6f1f73c0
corubio <corubio@uc.cl>
2020-10-21 17:11:11 -0300

    testing

--- Entrega3/index.php
+++ Entrega3/index.php
@@ -3,7 +3,7 @@
   <body>
   <img src='templates/animated.gif'/>
   <br>
-  <h2 data-wow-delay=".1s">Booking UC</h2>
+  <h2 data-wow-delay=".1s">Puertos y Navieras</h2>
   <a href="login.php">Iniciar sesión</a>
   </body>
 </html>
