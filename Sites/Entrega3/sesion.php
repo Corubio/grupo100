43a12f8031548215700af047642955876b6bf72a
corubio <corubio@uc.cl>
2020-10-23 17:18:53 -0300

    perfil

--- Sites/Entrega3/sesion.php
+++ Sites/Entrega3/sesion.php
@@ -7,7 +7,9 @@
 <html class="wide wow-animation" lang="en">
   <body>
   <h2 data-wow-delay=".1s">Que deseas hacer?</h2>
-  <li><a href="/perfil/perfil.php">Mi perfil</a></li>
+  <form action="/~grupo100/Entrega3/perfil/perfil.php" method="post">
+  <li><input type="submit" value="Mi perfil" ></li>
+  </form>
   <li><a href="/perfil/cambio_contraseña.php">Cambiar contraseña</a></li>
   <li><a href="/consultas/navieras_buques.php">Consultar por navieras y buques</a></li>
   <li><a href="/consultas/puertos.php">Consultar por puertos</a></li>
