0d14d6fce336c04e502aa82788c3616e7e733593
corubio <corubio@uc.cl>
2020-10-22 21:59:31 -0300

    sesion

--- Sites/Entrega3/sesion.php
+++ Sites/Entrega3/sesion.php
@@ -5,7 +5,7 @@
     session_start();
     $usuario = $_SESSION['usuario'];
     echo "Bienvenido a Puertos y Navieras ".$usuario."";
-    ?>
+  ?>
   <h2 data-wow-delay=".1s">Que deseas hacer?</h2>
   <li><a href="/perfil/perfil.php">Mi perfil</a></li>
   <li><a href="/perfil/cambio_contraseña.php">Cambiar contraseña</a></li>
