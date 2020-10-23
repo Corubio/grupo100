d7250d851bb9da0241ca8829ce3e26865f3cd490
corubio <corubio@uc.cl>
2020-10-23 19:21:40 -0300

    son capitanes?

--- Sites/Entrega3/perfil/perfil.php
+++ Sites/Entrega3/perfil/perfil.php
@@ -18,25 +18,25 @@
   ?>
 <?php
   foreach ($log2 as $capitan) {
-    if ($capitan[0] == '1') {
-      require("../acciones/conectar.php");
-        $query3 = "SELECT (buques.nombre, buques.patente, buques.tipo, navieras.nombre) FROM usuarios JOIN personal ON usuarios.nombre=personal.nombre RIGHT JOIN buques ON personal.pid = buques.id_capitan RIGHT JOIN navieras ON buques.nid = navieras.nid WHERE usuarios.nombre='$usuario';";
-        $result3 = $db -> prepare($query);
-        $result3 -> execute();
-        $log3 = $result3 -> fetchAll();
-      foreach ($log3 as $datos) {
-        $_SESSION['buque'] = $datos[0];
-        $_SESSION['patente'] = $datos[1];
-        $_SESSION['tipo'] = $datos[2];
-        $_SESSION['naviera'] = $datos[3];
-      }
-    } else {
-      $_SESSION['buque'] = 'Usted no es capitán';
-      $_SESSION['patente'] = 'Usted no es capitán';
-      $_SESSION['tipo'] = 'Usted no es capitán';
-      $_SESSION['naviera'] = 'Usted no es capitán';
+  if ($capitan[0] == '1') {
+    require("../acciones/conectar.php");
+      $query3 = "SELECT (buques.nombre, buques.patente, buques.tipo, navieras.nombre) FROM usuarios JOIN personal ON usuarios.nombre=personal.nombre RIGHT JOIN buques ON personal.pid = buques.id_capitan RIGHT JOIN navieras ON buques.nid = navieras.nid WHERE usuarios.nombre='$usuario';";
+      $result3 = $db -> prepare($query);
+      $result3 -> execute();
+      $log3 = $result3 -> fetchAll();
+    foreach ($log3 as $datos) {
+      $_SESSION['buque'] = $datos[0];
+      $_SESSION['patente'] = $datos[1];
+      $_SESSION['tipo'] = $datos[2];
+      $_SESSION['naviera'] = $datos[3];
     }
+  } else {
+    $_SESSION['buque'] = 'Usted no es capitán';
+    $_SESSION['patente'] = 'Usted no es capitán';
+    $_SESSION['tipo'] = 'Usted no es capitán';
+    $_SESSION['naviera'] = 'Usted no es capitán';
   }
+}
   foreach ($log as $datos) {
     $_SESSION['edad'] = $datos[2];
     $_SESSION['sexo'] = $datos[3];
