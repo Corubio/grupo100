f3b9fb5ea65276477a3e47ec7ee0dc03f967affd
corubio <corubio@uc.cl>
2020-10-26 00:27:32 -0300

    consulta 1

--- Sites/Entrega3/consultas/naviera_consultada.php
+++ Sites/Entrega3/consultas/naviera_consultada.php
@@ -1,5 +1,4 @@
 <?php
-  session_start();
   $naviera = $_POST['naviera'];
   ?>
 <?php
--- Sites/Entrega3/consultas/navieras_buques.php
+++ Sites/Entrega3/consultas/navieras_buques.php
@@ -11,8 +11,8 @@
 <?php
   foreach ($log as $naviera) {
     echo '<form action="/~grupo100/Entrega3/consultas/naviera_consultada.php" method="post>
-          <input id="naviera" value='.$naviera['nombre'].'>
-          <input type="submit" value='.$naviera['nombre'].'>
+          <input id="naviera" name="naviera" value='.$naviera[0].'>
+          <input type="submit" value='.$naviera[0].'>
           </form>';
   }
   ?>
