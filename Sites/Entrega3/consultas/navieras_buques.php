63bae66a392eaaa3dcbc01b1f8e97e13914033e1
corubio <corubio@uc.cl>
2020-10-26 00:32:22 -0300

    consulta 1

--- Sites/Entrega3/consultas/navieras_buques.php
+++ Sites/Entrega3/consultas/navieras_buques.php
@@ -11,8 +11,7 @@
 <?php
   foreach ($log as $naviera) {
     echo '<form action="/~grupo100/Entrega3/consultas/naviera_consultada.php" method="post>
-          <input id="naviera" name="naviera" value='.$naviera[0].'>
-          <input type="submit" value='.$naviera[0].'>
+          <input id="naviera" name="naviera" type="submit" value='.$naviera[0].'>
           </form>';
   }
   ?>
