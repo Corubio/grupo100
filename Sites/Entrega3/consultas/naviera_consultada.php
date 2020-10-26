a9d72244facab62686feede040b0667f78ccb506
corubio <corubio@uc.cl>
2020-10-26 00:51:56 -0300

    consulta 1

--- Sites/Entrega3/consultas/naviera_consultada.php
+++ Sites/Entrega3/consultas/naviera_consultada.php
@@ -2,5 +2,14 @@
   $naviera = $_GET['naviera'];
   ?>
 <?php
-  echo ''.$naviera.'';
+  require("../acciones/conectar.php");
+    $query = "SELECT * FROM buques WHERE buques.nid=$naviera ORDER BY buques.tipo DESC;";
+    $result = $db -> prepare($query);
+    $result -> execute();
+    $log = $result -> fetchAll();
+  echo "".$log."<br>";
+  foreach ($log as $buque) {
+    echo "".$buque."<br>";
+  }
+  echo '<br><a href="../sesion.php">Volver</a>';
   ?>
--- Sites/Entrega3/consultas/navieras_buques.php
+++ Sites/Entrega3/consultas/navieras_buques.php
@@ -3,7 +3,7 @@
   ?>
 <?php
   require("../acciones/conectar.php");
-    $query = "SELECT navieras.nombre FROM navieras;";
+    $query = "SELECT navieras.nombre, navieras.nid FROM navieras;";
     $result = $db -> prepare($query);
     $result -> execute();
     $log = $result -> fetchAll();
@@ -11,9 +11,10 @@
 <?php
   echo '<form action="/~grupo100/Entrega3/consultas/naviera_consultada.php" method="get">';
   foreach ($log as $naviera) {
-    echo '<input type="radio" id="'.$naviera[0].'" name="naviera" value="'.$naviera[0].'">
-          <label for="'.$naviera[0].'">'.$naviera[0].'</label><br>';
+    echo '<input type="radio" id="'.$naviera[1].'" name="naviera" value="'.$naviera[1].'">
+          <label for="'.$naviera[1].'">'.$naviera[0].'</label><br>';
   }
   echo '<input type="submit" value="Buscar">';
   echo '</form>';
+  echo '<br><a href="../sesion.php">Volver</a>';
   ?>
