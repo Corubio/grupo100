274cbdb77b0a8c94df4fafec04a1ba6cb08aaa49
NOMBRE APELLIDO <correo@foo.com>
2020-10-27 18:19:16 -0300

    funcionalidad itinerario

--- Sites/Entrega3/ingresar_itinerario.php
+++ Sites/Entrega3/ingresar_itinerario.php
@@ -23,6 +23,7 @@
     echo "Ingrese el nuevo itinerario:";
     echo "<br>";
     echo "<form action='/~grupo100/Entrega3/acciones/itinerario.php' method='post'>";
+    echo '<input type="date" name="fecha">';
     echo "<label for='fecha'>Nuevo Itinerario</label>";
     echo '<input type="text" name="lugar"> <input type="submit" value="Enviar datos"></p>';
     echo "<label for='lugar'>Lugar de atraque</label>";
