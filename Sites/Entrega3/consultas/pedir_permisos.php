6c5eb151bb0671ff1a69c46ecba741a5dfe911bc
Sebarraza <smbarraza@uc.cl>
2020-10-27 18:16:56 -0300

    tabla sin header

--- Sites/Entrega3/consultas/pedir_permisos.php
+++ Sites/Entrega3/consultas/pedir_permisos.php
@@ -9,25 +9,36 @@
 ?>
 
 <form action="./consulta_permiso.php" method="get">
-  <?php echo '<input type="hidden" value="$id">'; ?>
-
+  <?php echo '<input type="hidden" value="'.$id.'">'; ?>
+<table>
+  <tr>
   <label for="tipo">Eliga tipo de Instalacion</label>
   <select name="tipo" id="tipo">
     <option value="muelle">Muelle</option>
     <option value="astillero">Astillero</option>
   </select>
+  </tr>
 
+  <tr>
   <label for="fecha_in">Fecha Ingreso</label>
   <input type="date" id="fecha_in" name="fecha_in">
+  </tr>
 
+  <tr>
   <label for="fecha_out">Fecha Salida</labe>
   <input type="date" id="fecha_out" name="fecha_out">
+  </tr>
 
+  <tr>
   <label for="patente">Ingrese patente del barco</label>
   <input type="text" id="patente" name="patente">
+  </tr>
 
+  <tr>
   <input type="reset" value="Reset">
   <input type="submit" value="Realizar Solicitud">
+  </tr>
+</table>
 </form>
 
 
