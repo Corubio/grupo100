19ddcfb79fb0430183bcb4004d3a703474088478
Sebarraza <smbarraza@uc.cl>
2020-10-27 18:12:53 -0300

    test permisos

--- Sites/Entrega3/consultas/pedir_permisos.php
+++ Sites/Entrega3/consultas/pedir_permisos.php
@@ -4,6 +4,32 @@
 
 <h2>Permisos:</h2>
 
+<?php
+  $id = $_GET["pid"];
+?>
+
+<form action="./consulta_permiso.php" method="get">
+  <?php echo '<input type="hidden" value="$id">'; ?>
+
+  <label for="tipo">Eliga tipo de Instalacion</label>
+  <select name="tipo" id="tipo">
+    <option value="muelle">Muelle</option>
+    <option value="astillero">Astillero</option>
+  </select>
+
+  <label for="fecha_in">Fecha Ingreso</label>
+  <input type="date" id="fecha_in" name="fecha_in">
+
+  <label for="fecha_out">Fecha Salida</labe>
+  <input type="date" id="fecha_out" name="fecha_out">
+
+  <label for="patente">Ingrese patente del barco</label>
+  <input type="text" id="patente" name="patente">
+
+  <input type="reset" value="Reset">
+  <input type="submit" value="Realizar Solicitud">
+</form>
+
 
 
 <br><a href="../puertos.php">Volver</a>
