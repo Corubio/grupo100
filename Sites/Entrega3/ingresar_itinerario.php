32f7b411753c6d0dee6992d1c303fccd197c581f
NOMBRE APELLIDO <correo@foo.com>
2020-10-27 14:31:33 -0300

    funcionalidad itinerarios

--- Sites/Entrega3/ingresar_itinerario.php
+++ Sites/Entrega3/ingresar_itinerario.php
@@ -11,20 +11,25 @@
   $naviera = $_SESSION['naviera'];
   $proximo = $_SESSION['proximo'];
   $previos = $_SESSION['previos'];
-  echo "Nombre: ".$usuario."";
-  echo "<br>";
-  echo "Buque: ".$buque."";
-  echo "<br>";
-  echo "Patente: ".$patente."";
-  echo "<br>";
-  echo "Tipo: ".$tipo."";
-  echo "<br>";
-  echo "Naviera: ".$naviera."";
-  echo "<br>";
-  echo "Proximo Itinerario: ".$proximo."";
-  echo "<br>";
-  echo "Ultimos puertos: ".$previos."";
-  echo '<input type="date" name="fecha"> <input type="submit" value="Enviar datos"></p>';
-  echo '</form>';
+  if ($buque == 'Usted no es capitán') {
+    echo "Nombre: ".$usuario."";
+    echo "<br>";
+    echo "Buque: ".$buque."";
+    echo "<br>";
+    echo "Patente: ".$patente."";
+    echo "<br>";
+    echo "Tipo: ".$tipo."";
+    echo "<br>";
+    echo "Naviera: ".$naviera."";
+    echo "<br>";
+    echo "Proximo Itinerario: ".$proximo."";
+    echo "<br>";
+    echo "Ultimos puertos: ".$previos."";
+    echo '<input type="date" name="fecha"> <input type="submit" value="Enviar datos"></p>';
+    echo '</form>';
+  }
+  else {
+    echo "No eres capitán, por lo que no puedes realizar esta funcionalidad";
+  }
   echo '<br><a href="../sesion.php">Volver</a>';
   ?>
